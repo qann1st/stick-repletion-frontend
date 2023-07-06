663fd6252990bb59238404fb1887e06bbf5986b3
Vladislav <bllxy67@gmail.com>
2023-07-06 16:23:37 +0300

    feat: create ui component Box

---
+++ src/shared/ui/Box/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+import classNames from 'classnames';
+import { FC, ReactNode } from 'react';
+import styles from './Box.module.css';
+
+interface BoxProps {
+  className?: string;
+  children?: ReactNode;
+  as: keyof JSX.IntrinsicElements;
+}
+
+export const Box: FC<BoxProps> = ({
+  className,
+  children,
+  as: C = 'div',
+  ...rest
+}) => (
+  <C {...rest} className={classNames(styles.box, className)}>
+    {children}
+  </C>
+);
